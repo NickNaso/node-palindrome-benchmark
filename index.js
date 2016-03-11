1a633eb50dac6e28cdc114e1bd0c06484dd2211f
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-03-11 20:29:45 +0100

    Check one liner implementation

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var palindromeC = require('palindrome');
 var palindromeJs = require('./palindrome.js');
+var palindromeOneLiner = require('./palindrome_one_liner.js');
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
-suite.add('Javascript palindrome', function() {
-  palindromeJs('a man a plan a cat a ham a yak a yam a hat a canal panama');
-})
-.add('C palindrome', function() {
+suite.add('C palindrome', function() {
   palindromeC('a man a plan a cat a ham a yak a yam a hat a canal panama');
+})
+.add('Javascript palindrome one liner', function() {
+  palindromeOneLiner('a man a plan a cat a ham a yak a yam a hat a canal panama');
+})
+.add('Javascript palindrome', function() {
+  palindromeJs('a man a plan a cat a ham a yak a yam a hat a canal panama');
 })
 .on('cycle', cycle)
 .on('complete', complete)
@@ -17,6 +21,7 @@ function cycle(event) {
   console.log(String(event.target));
 }
 
-function complete() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
+function complete(a,b) {
+  console.log('Fastest: ' + this.filter('fastest').map('name'));
+  console.log('Slowest: ' + this.filter('slowest').map('name'));
 }
